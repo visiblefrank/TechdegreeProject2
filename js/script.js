dd60145a49894bc43ed111e3b95e42b7f9f7686c
Visible <sites@visible.ie>
2021-02-23 17:35:40 +0000

    Slack Request

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@ Frank Keane
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-let theList = document.querySelectorAll("li");
+const theList = document.querySelectorAll("li");
+let theFilteredList = [];
 const perPage = 10;
 const pageContainer = document.querySelector(".page"); //this variable global so searchBar and appendPageLinks can see it
+
 //function to hide or display 'pages' - sections of 10(perPage) list items
 function showPage(list, section) {
   const startIndex = section * perPage - perPage;
@@ -22,13 +24,12 @@ function showPage(list, section) {
 }
 
 //function to generate, append, and add functionality to the pagination buttons.
-function appendPageLinks(list) {
-  const pageList = Math.ceil(list.length / perPage);
+function appendPageLinks(linkList) {
+  const pageList = Math.ceil(linkList.length / perPage);
   const ul = document.createElement("ul");
   const button = document.createElement("button");
   pageContainer.appendChild(ul);
   //loop through the list of pages & create a button for each page unless there is only 1 page to display
-  console.log(pageList);
   for (let i = 1; i <= pageList; i++) {
     const li = document.createElement("li");
     const button = document.createElement("a");
@@ -86,33 +87,27 @@ function searchBar() {
   errorContainer.style.textAlign = "center";
   errorP.style.color = "#4ba6c3";
   errorP.className = "error-p";
-  //create an array including all student names
-  studentArray = [];
-  searchArray = [];
-  let studentNames = document.getElementsByTagName("H3");
-  for (let i = 0; i < studentNames.length; i++) {
-    var currentName = studentNames[i];
-    studentArray.push(currentName.textContent);
-  }
   // add search function to button
+
+  let studentH3 = document.getElementsByTagName("H3");
   searchBtn.addEventListener("click", () => {
     let searchTerm = searchInput.value;
-    let pageDiv = document.getElementsByClassName("page");
-    let paginationList = document.querySelector(".pagination");
-    // loop through student details and compare search input to student names
     for (let i = 0; i < theList.length; i++) {
-      theList[i].style.display = "none";
-      if (studentArray[i].includes(searchTerm)) {
-        theList[i].style.display = "block";
-        searchArray.push(studentArray[i]);
+      let currentName = studentH3[i].innerHTML;
+      //console.log(currentName);
+      if (currentName.includes(searchTerm)) {
         match = 1;
+        theFilteredList.push(studentH3[i].parentNode.parentNode);
       } //if
     } //for
+    // console.log(theFilteredList);
+    // console.log(theList);
+    showPage(theFilteredList, 1);
     if (match == 0) {
       errorP.textContent = "Sorry, no results have been found.";
-    }
-    pageContainer.removeChild(paginationList);
-    appendPageLinks(searchArray);
+      pageContainer.removeChild(paginationList);
+      appendPageLinks(searchArray);
+    } //if
   }); //listener
 } //searchBar()
 
